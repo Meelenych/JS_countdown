030dd6ee6a26ab8543a50a66f01cacbd644b14d7
Meelenych <meelenych@gmail.com>
2021-09-15 13:27:53 +0300

    class added

--- index.js
+++ index.js
@@ -3,41 +3,50 @@ import refs from './refs.js'
 const { daysContent, hoursContent, minsContent, secsContent } = refs
 // console.log(daysContent, hoursContent, minsContent, secsContent);
 
-const finishDate = new Date("Dec 31, 2021").getTime()
-// console.log(finishDate);
+const targetDate = new Date("Dec 31, 2021").getTime()
+// console.log(targetDate);
 
 const currentDate = Date.now()
 // console.log(currentDate);
 
-function start() {
-    setInterval(() => {
-    let currentDate = Date.now()
-    let time = finishDate - currentDate
+let selector = document.getElementById("timer-1")
+// console.log(selector);
 
-        let days = Math.floor(time / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-        let secs = Math.floor((time % (1000 * 60)) / 1000);
+class CountdownTimer {
+    constructor({selector, targetDate}) {
+        setInterval(() => {
+            let currentDate = Date.now()
+            let time = targetDate - currentDate
 
-        days = String(days).padStart(3, "-")
-        // console.log("days:", days);        
-        hours = String(hours).padStart(2, "0")
-        // console.log("hours:", hours);
-        mins = String(mins).padStart(2, "0")
-        // console.log("minutes:", mins);
-        secs = String(secs).padStart(2, "0")
-        // console.log("seconds:", secs);
+            let days = Math.floor(time / (1000 * 60 * 60 * 24));
+            let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+            let secs = Math.floor((time % (1000 * 60)) / 1000);
 
-        daysContent.textContent = days
-        hoursContent.textContent = hours
-        minsContent.textContent = mins
-        secsContent.textContent = secs
+            days = String(days).padStart(3, "-")
+            // console.log("days:", days);        
+            hours = String(hours).padStart(2, "0")
+            // console.log("hours:", hours);
+            mins = String(mins).padStart(2, "0")
+            // console.log("minutes:", mins);
+            secs = String(secs).padStart(2, "0")
+            // console.log("seconds:", secs);
 
-    }, 1000);
-  
+            daysContent.textContent = days
+            hoursContent.textContent = hours
+            minsContent.textContent = mins
+            secsContent.textContent = secs
+
+        }, 1000);  
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function(e) {
     console.log("DOM loaded");
-    start()
-  });
+
+    new CountdownTimer({
+        selector: '#timer-1',
+        targetDate: new Date('Jul 17, 2022'),
+    });
+
+});
